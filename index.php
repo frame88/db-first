252993c2caf463f1030b9e78865fc5ce3124bbc9
frame88 <fleuzzi97@gmail.com>
2022-02-09 18:20:21 +0100

    consegna

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- Esercizio di oggi:
+nome repo: db-first
+Modellizzare (descriviamo come abbiamo fatto in classe) la struttura di una tabella per memorizzare tutti i dati riguardanti delle auto usate messe in vendita da un concessionario.
+Potete usare diagram ed esportare il file in formato pdf o scrivere un txt e poi inserirlo nella repo.
+Domani mattina non ci sarà lezione, ma avrete il seguente esercizio:
+1. Terminare lo snack iniziato stamattina rendendo effettivo il filtro con l’utilizzo del form prendete spunto da qui https://github.com/ChiaraPassaro/classe49-php-dischi
+2. In un nuovo progetto php creare un array che descriva tante auto come dati estrapolati dalla tabella del DB che avete modellizzato. -->
